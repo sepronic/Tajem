8702107185f26b14150ed7effe2ad5f96a460a36
sepronic <olha.prozoria@ua.fm>
2019-12-02 16:09:31 +0300

    create header slider

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
+})
+
