5cda514777183ab5d604e8650a804f181a195293
sepronic <olha.prozoria@ua.fm>
2019-12-17 12:20:45 +0300

    modify hamburger menu properties

--- js/script.js
+++ js/script.js
@@ -17,13 +17,11 @@ $(document).ready(function() {
         arrows: true,
         dots: true,
         fade: true,
-    });
-
-    $('.video__button').on('click', function() {
-        $(this).toggleClass('hide');
-        $('#video__play').toggleClass('show');
-    }); 
+    });  
 });
 
- 
+$('.hamburger__link').on('click', function(){
+    console.log('Вы нажали на элемент "hamburger__link"');
+    $('.menu__btn').trigger('click');
+});
 
